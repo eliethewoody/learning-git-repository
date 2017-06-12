1742e88aaa34013061f398f2edd6cc4f2eab600d
Ivanov Dmitry <errfrom@yandex.ru>
2017-06-12 20:15:19 +0300

    Some useless stuff was removed

--- learning-git/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to learning-git
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- learning-git/project.clj
+++
@@ -1,4 +0,0 @@
-(defproject learning-git "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
